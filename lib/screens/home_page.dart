8c85e03404c1ce2e586d5bd07e2819b9cb95a3c4
Muhammad Ahmed Elsaqa <saqaaa@outlook.com>
2020-10-26 14:00:29 +0200

    Adding background image

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tokyo/screens/numbers_page.dart';
 import 'package:tokyo/screens/familymembers_page.dart';
@@ -9,7 +10,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        backgroundColor: Colors.black87,
+        elevation: 100,
+        backgroundColor: Colors.black54,
         centerTitle: true,
         title: Text("Tokyo",
           style: TextStyle(
@@ -19,106 +21,115 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage("https://www.gooverseas.com/sites/default/files/styles/1014x/public/image-collections/2017-05-20/DSC_4493.jpg?itok=NROGOELV"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context){
+                      return NumbersPage();
+                    }
+                ));
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 10),
+                width: double.infinity,
+                height: 110,
+                color: Color(0xFFFF9887),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Numbers",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
                   builder: (context){
-                    return NumbersPage();
+                    return FamilyMembers();
                   }
-              ));
-            },
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              width: double.infinity,
-              height: 110,
-              color: Color(0xFFFF9887),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Numbers",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black87,
+                ));
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 10),
+                width: double.infinity,
+                height: 110,
+                color: Color(0xFFBBE5A4),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Family Members",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context){
-                  return FamilyMembers();
-                }
-              ));
-            },
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              width: double.infinity,
-              height: 110,
-              color: Color(0xFFBBE5A4),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Family Members",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black87,
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return ColorsPage();
+                  }
+                ));
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 10),
+                width: double.infinity,
+                height: 110,
+                color: Color(0xFFF9D699),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Colors",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context){
-                  return ColorsPage();
-                }
-              ));
-            },
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              width: double.infinity,
-              height: 110,
-              color: Color(0xFFF9D699),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Colors",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black87,
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return PhrasesPage();
+                  }
+                ));
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 10),
+                width: double.infinity,
+                height: 110,
+                color: Color(0xFFFFBF87),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Phrases",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context){
-                  return PhrasesPage();
-                }
-              ));
-            },
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              width: double.infinity,
-              height: 110,
-              color: Color(0xFFFFBF87),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Phrases",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
